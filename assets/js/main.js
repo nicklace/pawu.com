161f45a3bb75f5eaccd76a9ad03f4b9629198d65
Nicolas Delrez <nicolas@65inches.com>
2022-03-14 13:45:15 +0100

    [Add style]

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,149 +1,16 @@
 jQuery(document).ready(function () {
-
-  if($(".swiper-container").length){
-    const swiper = new Swiper('.swiper-container', {
-      // Optional parameters
-      loop: true,
-      effect: "fade",
-
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-
-    });
-  }
-
-  $(".chatTrigger").on('click', function(event) {
-    event.preventDefault();
-    $('#chat').toggleClass('show');
-  });
-
-  /* Menu */
-  $('.navbar').on('shown.bs.collapse', function () {
-    $('body').addClass('open');
-  });
-  $('.navbar').on('hidden.bs.collapse', function () {
-    $('body').removeClass('open');
-  })
-
-
-  var vpWidth = $( window ).width();
-
-  if(vpWidth>990){
-    openMenu();
-  }
-
-  if (vpWidth<1025 ){
-    subMenuMobile();
-  }
-
-  $( window ).resize(function() {
-    var vpWidth = $( window ).width();
-    if (vpWidth<1025 ){
-      subMenuMobile();
+  /* Scroll */
+  jQuery(".scroll").on('click', function(event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        jQuery('html, body').animate({
+        scrollTop: jQuery(hash).offset().top
+        }, 500, function(){
+        window.location.hash = hash;
+        });
     }
-    if(vpWidth>990){
-      openMenu();
-    }
-  });
-
-  // when any modal is opening
-  $('.modal').on('show.bs.modal', function (e) {
-    $('html').addClass('modal-opened');
-  })
-
-  // when any modal is closing
-  $('.modal').on('hide.bs.modal', function (e) {
-    $('html').removeClass('modal-opened');
-  })
-
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
-
-  function subMenuMobile(){
-
-    if (vpWidth<1025 && vpWidth>767){
-      $(".companiesPlaceHolder").wrap("<div class='col-6 companiesPlaceHolderParent fakeDiv'></div>");
-      $(".companiesHolder").detach().insertBefore(".companiesPlaceHolderParent").addClass('fakeDiv');
-      $( ".fakeDiv" ).wrapAll( "<div class='row' ></div>");
-    }
-
-    $(".navbar-collapse.collapse:not(.show) .dropdown-toggle").on('click', function(event) {
-      event.preventDefault();
-      $('body').addClass('open');
-      $('.navbar-collapse.collapse').addClass('show');
-      $('.navbar-toggler').removeClass('collapsed');
-    });
-
-  }
-
-  function openMenu(){
-    $('.showOnly').each(function(){
-      $(this).addClass('show');
-    });
-  }
-
-
-  /* Select */
-  jQuery('select').each(function(){
-      var jQuerythis = jQuery(this), numberOfOptions = jQuery(this).children('option').length;
-
-      jQuerythis.addClass('select-hidden'); 
-      jQuerythis.wrap('<div class="select"></div>');
-      jQuerythis.after('<div class="select-styled"></div>');
-
-      var jQuerystyledSelect = jQuerythis.next('div.select-styled');
-      jQuerystyledSelect.text(jQuerythis.children('option').eq(0).text());
-
-      var jQuerylist = jQuery('<ul />', {
-          'class': 'select-options'
-      }).insertAfter(jQuerystyledSelect);
-
-      for (var i = 1; i < numberOfOptions; i++) {
-          jQuery('<li />', {
-              text: jQuerythis.children('option').eq(i).text(),
-              rel: jQuerythis.children('option').eq(i).val()
-          }).appendTo(jQuerylist);
-      }
-
-      var jQuerylistItems = jQuerylist.children('li');
-
-      jQuerystyledSelect.click(function(e) {
-          e.stopPropagation();
-          jQuery('div.select-styled.active').not(this).each(function(){
-              jQuery(this).removeClass('active').next('ul.select-options').hide();
-          });
-          jQuery(this).toggleClass('active').next('ul.select-options').toggle();
-      });
-
-      jQuerylistItems.click(function(e) {
-          e.stopPropagation();
-          jQuerystyledSelect.text(jQuery(this).text()).removeClass('active');
-          jQuerythis.val(jQuery(this).attr('rel'));
-          jQuerylist.hide();
-          //console.log(jQuerythis.val());
-      });
-
-      jQuery(document).click(function() {
-          jQuerystyledSelect.removeClass('active');
-          jQuerylist.hide();
-      });
-
-  });
+});
 
 });
 
---
+++ index.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" type="image/png" href="favicon.png" />
+    
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <!-- Main CSS -->
+        <link rel="stylesheet" href="assets/css/main.css">
+    
+        <title>pawu</title>
+      </head>
+    <body>
+        <svg width="0" height="0" class="hidden">
+            <symbol viewBox="0 0 20 21" xmlns="http://www.w3.org/2000/svg" id="dialog">
+                <path d="M16.48 1.4287e-05H8.56C7.63428 0.00263667 6.74708 0.370859 6.09155 1.02451C5.43603 1.67816 5.06527 2.56431 5.06 3.49002V6.65002H3.5C2.57336 6.65527 1.68617 7.0257 1.03093 7.68094C0.375689 8.33618 0.0052522 9.22338 2.00329e-06 10.15V19.01C-0.000373071 19.1974 0.0519275 19.3812 0.150942 19.5403C0.249956 19.6994 0.391697 19.8276 0.56 19.91C0.697132 19.9764 0.847632 20.0106 1 20.01C1.22557 20.0087 1.44406 19.9312 1.62 19.79L4.16001 17.79H10.43C11.3566 17.7848 12.2438 17.4143 12.8991 16.7591C13.5543 16.1038 13.9248 15.2167 13.93 14.29V11.11H15.83L18.36 13.11C18.5073 13.228 18.6851 13.3019 18.8727 13.3232C19.0602 13.3444 19.25 13.3121 19.42 13.23C19.5902 13.149 19.734 13.0215 19.8348 12.8622C19.9357 12.703 19.9895 12.5185 19.99 12.33V3.50001C19.99 3.03955 19.8991 2.5836 19.7226 2.15831C19.5461 1.73302 19.2874 1.34673 18.9614 1.0216C18.6353 0.696462 18.2483 0.438863 17.8225 0.26356C17.3967 0.0882579 16.9405 -0.00130134 16.48 1.4287e-05ZM11.91 14.25C11.9038 14.6485 11.7423 15.0288 11.46 15.31C11.195 15.5788 10.8373 15.7362 10.46 15.75H3.84C3.61328 15.7444 3.39249 15.8228 3.22 15.97L2.03 16.91V10.13C2.03 9.73219 2.18804 9.35066 2.46935 9.06936C2.75065 8.78805 3.13218 8.63001 3.53 8.63001H10.95C11.0823 8.63056 11.2132 8.65736 11.3351 8.70886C11.4569 8.76036 11.5674 8.83553 11.66 8.93001C11.8447 9.11627 11.9489 9.36767 11.95 9.63001L11.91 14.25ZM17.97 10.25L16.79 9.25001C16.6114 9.10671 16.389 9.02905 16.16 9.03001H13.86C13.7547 8.42895 13.4687 7.87428 13.04 7.44001C12.4779 6.87724 11.7154 6.56072 10.92 6.56002H7.05V3.48001C7.05265 3.08392 7.21184 2.70496 7.49286 2.42581C7.77388 2.14667 8.15391 1.99001 8.55 1.99002H16.47C16.8678 1.99002 17.2494 2.14805 17.5307 2.42935C17.812 2.71066 17.97 3.09219 17.97 3.49002V10.25Z" fill="white"/>
+            </symbol>  
+            <symbol viewBox="0 0 20 22" xmlns="http://www.w3.org/2000/svg" id="arrow">
+                <path d="M9.98505 1V21" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M19 9.53064C13.99 11.4383 11.035 15.2614 10 20.9999" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M1 9.53064C6.0175 11.4383 8.9725 15.2614 10 20.9999" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </symbol>
+
+        </svg>
+        <header class="container">
+            <img src="assets/svg/logo.svg" alt="">         
+            <a href="" class="btn"><svg class="icon" width="20" height="21"><use xlink:href="#dialog"></use></svg>Discussion group</a>
+        </header>
+        <main class="container">
+            <section id="hero">
+                <div class="row">
+                    <div class="col-md-5 text">
+                        <h5>Welcome to PAWU!</h5>
+                        <h1>Patient advocates of the world, unite!</h1>
+                    </div>
+                    <div class="col-md-7 visu">
+                        <img src="assets/svg/illu_header.svg" alt="">
+                    </div>
+                </div>
+                <a href="#about" class="btn btn--alone scroll"><svg class="icon" width="20" height="22"><use xlink:href="#arrow"></use></svg></a>
+            </section>
+            <section id="about">
+                <span class="special special--big" data-content="PAWU is">PAWU is</span>
+                <div class="row">
+                    <div class="col-4">
+                        <h2>Patients Advocates Worldwide United</h2>
+                    </div>
+                    <div class="col-8">
+                        <p>We are a loose group of patients advocates. No legal structure. Minimal costs. 
+                            Maximum efficiency. Everyone interested by the subject can join.</p>
+                        <p>Our only goal is to facilitate the communication between patient advocates. That’s it.</p>
+                        <p>And we want to be global, so advocates from various countries can learn from each other.</p>
+                        <p>There are many patients associations, groups, initiatives. 
+                            They are all our friends. <strong>We don’t compete. We collaborate.</strong></p>
+                    </div>
+                </div>
+                <h4>This is our website. Our domain name is</h4>
+                <span class="special " data-content="pawu.online">pawu.online</span>
+            </section>
+            <section id="join">
+                <img src="assets/svg/illu_bottom.svg" alt="">
+                <h3>Start by joining the discussion via our discussion group</h3>
+                <a href="" class="btn">Join us</a>
+            </section>
+        </main>
+        <footer>
+
+        </footer>
+
+        <!-- Optional JavaScript -->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
